8e163688d0e7cf6a8325253a6e68eb9277ade7ae
KevinVelasquezIES <kevin.velasquez.ies@gmail.com>
2023-11-18 11:05:04 -0500

    Feat: se agregan las animaciones y los eventos de teclado necesarios para el explorer aun sin desplazamientos

--- src/games/Explorer.ts
+++ src/games/Explorer.ts
@@ -40,8 +40,8 @@ export class Explorer extends Container {
       down: explorerFrames.slice(0).map(frame => Texture.from(frame)),
       left: explorerFrames.slice(3).map(frame => Texture.from(frame)),
       right: explorerFrames.slice(6).map(frame => Texture.from(frame)),
-      moveUp: explorerFrames.slice(0, 3).map(frame => Texture.from(frame)),
-      moveDown: explorerFrames.slice(9, 12).map(frame => Texture.from(frame)),
+      moveUp: explorerFrames.slice(9, 12).map(frame => Texture.from(frame)),
+      moveDown: explorerFrames.slice(0, 3).map(frame => Texture.from(frame)),
       moveLeft: explorerFrames.slice(3, 6).map(frame => Texture.from(frame)),
       moveLRight: explorerFrames.slice(6, 9).map(frame => Texture.from(frame)),
     };
@@ -53,6 +53,78 @@ export class Explorer extends Container {
 
     this.animatedExplorer.anchor.set(0.5);
 
+    //Inicializacion y llamado del evento para los teclados
     Keyboard.initialize();
+    window.addEventListener("keyChanged", this.handleKeyChange.bind(this));
+  }
+
+  private handleKeyChange(event: Event): void {
+    const keyEvent = event as CustomEvent;
+    const { key, isPressed } = keyEvent.detail as {
+      key: string;
+      isPressed: boolean;
+    };
+
+    if (isPressed) {
+      this.handleKeyPress(key);
+    } else {
+      this.handleKeyRelease(key);
+    }
+  }
+
+  private handleKeyPress(key: string): void {
+    switch (key) {
+      case "ArrowUp":
+        this.startAnimation(this.states.moveUp);
+        break;
+      case "ArrowDown":
+        this.startAnimation(this.states.moveDown);
+        break;
+      case "ArrowLeft":
+        this.startAnimation(this.states.moveLeft);
+        break;
+      case "ArrowRight":
+        this.startAnimation(this.states.moveLRight);
+        break;
+      default:
+        break;
+    }
+  }
+
+  private startAnimation(textures: Array<Texture>): void {
+    if (
+      !this.animatedExplorer.playing ||
+      this.animatedExplorer.textures !== textures
+    ) {
+      this.animatedExplorer.textures = textures;
+      this.animatedExplorer.loop = true;
+      this.animatedExplorer.animationSpeed = 0.2;
+      this.animatedExplorer.play();
+    }
+  }
+
+  private handleKeyRelease(key: string): void {
+    switch (key) {
+      case "ArrowUp":
+        this.startAnimation(this.states.up);
+        this.animatedExplorer.stop();
+        break;
+      case "ArrowDown":
+        this.startAnimation(this.states.down);
+        this.animatedExplorer.stop();
+
+        break;
+      case "ArrowLeft":
+        this.startAnimation(this.states.left);
+        this.animatedExplorer.stop();
+
+        break;
+      case "ArrowRight":
+        this.startAnimation(this.states.right);
+        this.animatedExplorer.stop();
+        break;
+      default:
+        break;
+    }
   }
 }
--- src/utils/Keyboard.ts
+++ src/utils/Keyboard.ts
@@ -1,16 +1,28 @@
 export class Keyboard {
-  public static readonly state: Map<string, boolean>;
+  public static readonly state: Map<string, boolean> = new Map();
+  public static readonly keyPressedEvent = new Event("keyPressed");
+  public static readonly keyReleasedEvent = new Event("keyReleased");
+
   public static initialize() {
-    // The `.bind(this)` here isn't necesary as these functions won't use `this`!
     document.addEventListener("keydown", Keyboard.keyDown);
     document.addEventListener("keyup", Keyboard.keyUp);
   }
+
   private static keyDown(e: KeyboardEvent): void {
-    //Keyboard.state.set(e.code, true);
-    console.log("siento una presion", e);
+    Keyboard.state.set(e.code, true);
+    window.dispatchEvent(
+      new CustomEvent("keyChanged", {
+        detail: { key: e.code, isPressed: true },
+      })
+    );
   }
+
   private static keyUp(e: KeyboardEvent): void {
-    //Keyboard.state.set(e.code, false);
-    console.log("siento una depresion", e);
+    Keyboard.state.set(e.code, false);
+    window.dispatchEvent(
+      new CustomEvent("keyChanged", {
+        detail: { key: e.code, isPressed: false },
+      })
+    );
   }
 }
